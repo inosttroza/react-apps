1712e67a617de5cb048ec086febb4773e3ba0c3e
Pablo Inostroza <41707302+inosttroza@users.noreply.github.com>
2022-09-08 18:57:27 -0300

    Primer commit

---
+++ 6-react-table-component-separates/src/components/EditRow.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+
+const EditRow = ({
+  tableData,
+  Cancel,
+  setTableData,
+  editData,
+  setEditData,
+}) => {
+  const handleEditChange = (e) => {
+    const { name, value } = e.target;
+    setEditData({ ...editData, [name]: value });
+  };
+
+  const Save = (e) => {
+    e.preventDefault();
+    let filterData = tableData.filter((e) => e.id !== editData.id);
+    let updatedData = [...filterData, { ...editData }].sort((a, b) =>
+      a.id > b.id ? 1 : -1
+    );
+    setTableData(updatedData);
+    // const delData = tableData.filter((tbd) => {
+    //   return editData.id !== tbd.id;
+    // });
+    // setTableData(delData);
+  };
+  return (
+    <>
+      <tr>
+        <td>
+          <input
+            type="text"
+            name="fullName"
+            required
+            placeholder="Enter a name..."
+            onChange={handleEditChange}
+            value={editData.fullName}
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            name="userName"
+            required
+            placeholder="Enter user name..."
+            onChange={handleEditChange}
+            value={editData.userName}
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            name="phoneNumber"
+            required
+            placeholder="Enter a phone number..."
+            onChange={handleEditChange}
+            value={editData.phoneNumber}
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            name="website"
+            required
+            placeholder="Enter website..."
+            onChange={handleEditChange}
+            value={editData.website}
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            name="companyName"
+            required
+            placeholder="Enter company name..."
+            onChange={handleEditChange}
+            value={editData.companyName}
+          />
+        </td>
+        <td>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="Enter an email..."
+            onChange={handleEditChange}
+            value={editData.email}
+          />
+        </td>
+        <td className="d-flex justify-content-center">
+          <button className="btn btn-success" type="submit" onClick={Save}>
+            Guardar
+          </button>
+          <button className="btn btn-danger" type="button" onClick={Cancel}>
+            Eliminar
+          </button>
+        </td>
+      </tr>
+    </>
+  );
+};
+
+export default EditRow;
